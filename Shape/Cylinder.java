2dcd2e05505b69745884bdb3de7584048e953f07
v_shinde <139036307+1vedantshinde@users.noreply.github.com>
2025-03-05 14:07:37 +0530

    Create Cylinder.java

---
+++ Shape/Cylinder.java
@@ -0,0 +1,23 @@
+//Cylinder.java
+
+class Cylinder extends Shape {
+    private double radius, height;
+
+    public Cylinder(double radius, double height) {
+        super("Cylinder");
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public double calculateArea() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double calculateVolume() {
+        return Math.PI * radius * radius * height;
+    }
+}
