988daaf3704fc2e8adc864ffe442238b56fd7dc8
v_shinde <139036307+1vedantshinde@users.noreply.github.com>
2025-03-05 14:01:48 +0530

    Create Rectangle.java

---
+++ Shape/Rectangle.java
@@ -0,0 +1,22 @@
+//Rectangle.java
+class Rectangle extends Shape {
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+    
+    public double calculateVolume() {
+        return 0; 
+    }
+}
