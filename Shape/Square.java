a5895cbf156e5da6255d47cc4fc1c3ddb4eec252
v_shinde <139036307+1vedantshinde@users.noreply.github.com>
2025-03-05 13:58:30 +0530

    Create Square.java

---
+++ Shape/Square.java
@@ -0,0 +1,22 @@
+//Square.java
+
+class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        super("Square");
+        this.side = side;
+    }
+
+    public double calculateArea() {
+        return side * side;
+    }
+
+    public double calculatePerimeter() {
+        return 4 * side;
+    }
+    
+    public double calculateVolume() {
+        return 0; 
+    }
+}
