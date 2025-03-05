1bb8f1f45703eb29051f634a5f13a7f72e75d253
v_shinde <139036307+1vedantshinde@users.noreply.github.com>
2025-03-05 14:01:08 +0530

    Create Circle.java

---
+++ Shape/Circle.java
@@ -0,0 +1,22 @@
+//Circle.java
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    
+    public double calculateVolume() {
+        return 0;
+    }
+}
