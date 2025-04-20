d13a5378000bc5a1fa7823c225db214d243938a5
1vedantshinde <2vedsh@gmail.com>
2025-04-20 22:25:50 +0530

    Added Threads

---
+++ Threads/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        new Thread(new MyThread(1, 2,300)).start();
+        new Thread(new MyThread(100, 20, 100)).start();
+    }
+
+}
